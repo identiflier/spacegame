39cecb6cd9b87d9defecb2fef1cd22d72950dc89
Josh Rakes <josh.rakes0@gmail.com>
2023-10-30 19:54:24 -0600

    Added some features and a README file.

---
+++ README.txt
@@ -0,0 +1,9 @@
+This is just a fun little pet project by Josh Rakes.
+I have almost no experience with programming, python, or game development.
+The game is in a severely unfinished state right now.
+
+In the current state, the turret only works if you're using controller input.
+Apparently different controllers use different button mapping, so the controller input might not even work.
+
+WASD to move. Click to shoot. Scroll to zoom.
+Those are about all the controls there are so far.
--- main.py
+++ main.py
@@ -2,24 +2,31 @@ import pygame
 import math
 import random
 
-#This is a test change
-
 # Initialize Pygame
 pygame.init()
 
 # Set up the window
 
-'''
-WINDOW_WIDTH = 1400
-WINDOW_HEIGHT = 700
-'''
 
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 400
+
+
+# Choose windown mode (Comment out the mode you don't want)
 window = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+#window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
 window_width, window_height = pygame.display.get_surface().get_size()
 pygame.display.set_caption("Space Shooter")
 
+# Initialize the joysticks
+pygame.joystick.init()
+joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
+
 # Set some constants
 SPEED_OF_LIGHT = 10
+SHIP_ACCELERATION = 0.1
+TURRET_LENGTH = 15
 
 # Define some colors
 BLACK = (0, 0, 0)
@@ -83,14 +90,24 @@ class Camera:
             scaled_size = 1
         pygame.draw.circle(window, color, screen_pos, scaled_size)
 
+    def draw_line(self, world_pos1, world_pos2, color=WHITE, width=1):
+        screen_pos1 = self.world_to_screen(world_pos1)
+        screen_pos2 = self.world_to_screen(world_pos2)
+        pygame.draw.line(window, color, screen_pos1, screen_pos2, width)
+
 class Ship:
-    def __init__(self, position=[0, 0], accel = 0.01):
+    def __init__(self, position=[0, 0], accel = SHIP_ACCELERATION):
+        self.game = None
+        self.controller = None
+        self.turret_length = TURRET_LENGTH
         self.world_pos = position
         self.vel = [0, 0]
         #real_vel is the ship's speed after applying the speed of light limit
         self.real_vel = [0, 0]
         self.accel = accel
         self.radius = 10
+        self.angle = 0
+
 
     def update(self):
         keys = pygame.key.get_pressed()
@@ -104,12 +121,19 @@ class Ship:
             self.vel[0] -= self.accel
         if keys[pygame.K_d]:
             self.vel[0] += self.accel
-        
-        #adjust for speed of light limit
+
+        # Apply left joystick
+        self.vel[0] += self.controller.left_stick[0] * self.accel
+        self.vel[1] -= self.controller.left_stick[1] * self.accel
+
+        # Adjust for speed of light limit
         self.real_vel[0] = apply_sol(self.vel[0])
         self.real_vel[1] = apply_sol(self.vel[1])
 
-        # adjust position
+        # Adjust angle
+        self.angle = math.atan2(-self.controller.right_stick[1], self.controller.right_stick[0])
+
+        # Adjust position
         self.world_pos[0] += self.real_vel[0]
         self.world_pos[1] += self.real_vel[1]
 
@@ -123,23 +147,37 @@ class Bullet:
         self.world_pos[0] += self.vel[0]
         self.world_pos[1] += self.vel[1]
 
-
 class TestPoint:
     def __init__(self, position=[0, 0]):
         self.world_pos = position
         self.radius = 10
 
-p1 = TestPoint()
-p2 = TestPoint([500, 0])
-myship = Ship()
-bullets = []
+class Controller:
+    def __init__(self):
+        self.joystick = None
+        self.ship = None
+        self.buttons = []
+        self.axes = []
+        self.hats = []
+        self.left_stick = [0, 0]
+        self.right_stick = [0, 0]
+        self.right_trigger = -1
+        self.already_shot = False
+    
+    def update(self):
+        if self.right_trigger > 0 and not self.already_shot:
+            self.already_shot = True
+            bullets.append(Bullet(self.ship.world_pos.copy(), self.ship.angle))
+        if self.right_trigger <= 0:
+            self.already_shot = False
 
 class Game:
     def __init__(self):
         self.camera = Camera()
-        self.playership = myship
+        self.playership = Ship()
         self.camera.following = self.playership
         self.clock = pygame.time.Clock()
+        self.controller = Controller()
     
     def run(self):
         # Main game loop
@@ -169,10 +207,27 @@ class Game:
                         angle = math.atan2(click_world_pos[1] - pos[1], click_world_pos[0] - pos[0])
                         bullets.append(Bullet(pos, angle))
                 
+                if event.type == pygame.JOYBUTTONDOWN:
+                    print(event)
+                if event.type == pygame.JOYAXISMOTION:
+                    #print(event)
+                    if event.axis == 0:
+                        self.controller.left_stick[0] = event.value
+                    if event.axis == 1:
+                        self.controller.left_stick[1] = event.value
+                    if event.axis == 2:
+                        self.controller.right_stick[0] = event.value
+                    if event.axis == 3:
+                        self.controller.right_stick[1] = event.value
+                    if event.axis == 5:
+                        self.controller.right_trigger = event.value
+                if event.type == pygame.JOYHATMOTION:
+                    print(event)                
 
             # Update things
             self.camera.update()
             self.playership.update()
+            self.controller.update()
             for bullet in bullets:
                 bullet.update()
 
@@ -191,6 +246,10 @@ class Game:
         self.camera.draw_circle(p1.world_pos, 10)
         self.camera.draw_circle(p2.world_pos, 10)
         self.camera.draw_circle(self.playership.world_pos, self.playership.radius, GREEN)
+        turret_pos_x = self.playership.world_pos[0] + math.cos(self.playership.angle) * self.playership.turret_length
+        turret_pos_y = self.playership.world_pos[1] + math.sin(self.playership.angle) * self.playership.turret_length
+        self.camera.draw_line(self.playership.world_pos, [turret_pos_x, turret_pos_y], GREEN, int(5 * self.camera.zoom))
+        self.camera
         for bullet in bullets:
             self.camera.draw_circle(bullet.world_pos, bullet.radius, RED)
 
@@ -208,6 +267,17 @@ def apply_sol(speed):
     else:
         return sol * (1-math.e**(-slope*speed))
 
+p1 = TestPoint()
+p2 = TestPoint([500, 0])
+bullets = []
+
 # Start the game
 game = Game()
+
+# Attach objects together
+game.playership.game = game
+game.playership.controller = game.controller
+game.controller.ship = game.playership
+
+# Run the game
 game.run()
